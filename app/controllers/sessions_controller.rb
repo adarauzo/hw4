d1dae5529753a7531ded35b4af83b5b6ec74ca3d
Adrian Arauzo Vaillo <adrian.arauzovaillo@kellogg.northwestern.edu>
2024-03-09 21:06:58 +0000

    Website mostly works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to places_path
+    else
+      redirect_to login_path, notice: "Invalid email or password"
+    end
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to login_path
   end
 end
-  
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "places", :action => "index" })
+  root to: 'sessions#new'
   resources "entries"
   resources "places"
   resources "sessions"
